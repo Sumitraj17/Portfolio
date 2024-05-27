19a1b27e89cc92c5935b7a7f8985284748ae4f79
Sumit Raj <sumitraj1533@gmail.com>
2024-05-27 19:39:09 +0530

    Update Cors

--- portfolio/src/context/states.jsx
+++ portfolio/src/context/states.jsx
@@ -2,12 +2,9 @@ import react, { useState } from "react";
 import context from "./context.jsx";
 
 const States = (props)=>{
-    const [state,setState] = useState(()=>{
-        return localStorage.getItem('state') || 'About'
-    })
+    const [state,setState] = useState('About')
     const update = (state1)=>{
         setState(state1)
-        localStorage.setItem('state',state1);
     }
     return(
         <context.Provider value={{state,update}}>
--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'https://portfolio-server-zuv6.onrender.com:10000', // Replace with your actual API URL if necessary
+        target: 'https://portfolio-server-zuv6.onrender.com', // Replace with your actual API URL if necessary
         changeOrigin: true, // Enable CORS rewriting for development
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Rewrite paths for cleaner URLs
       },
