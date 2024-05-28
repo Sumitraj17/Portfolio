95fd47dc0c3745a763457fd6d86b26787b6aebee
Sumit Raj <sumitraj1533@gmail.com>
2024-05-28 10:08:12 +0530

    Update LocalStorage

--- portfolio/src/context/states.jsx
+++ portfolio/src/context/states.jsx
@@ -2,9 +2,12 @@ import react, { useState } from "react";
 import context from "./context.jsx";
 
 const States = (props)=>{
-    const [state,setState] = useState('About')
+    const [state,setState] = useState(()=>{
+        return localStorage.getItem('state') || 'About'
+    })
     const update = (state1)=>{
         setState(state1)
+        localStorage.setItem('state',state1);
     }
     return(
         <context.Provider value={{state,update}}>
